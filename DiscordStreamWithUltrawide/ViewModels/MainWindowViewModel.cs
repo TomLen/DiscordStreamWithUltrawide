34321e06ae13b3bb200028fa39ae0ef9bf617023
TomLen <77353941+TomLen@users.noreply.github.com>
2021-11-24 12:46:59 +0100

    init

---
+++ DiscordStreamWithUltrawide/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace DiscordStreamWithUltrawide.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+        private string _title = "Test Title";
+        public MainWindowViewModel()
+        {
+            
+        }
+        public string Title
+        {
+            get { return _title; } 
+            set { _title = value; } 
+        }
+    }
+}
